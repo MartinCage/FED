71888643fd876b088511972bdc8e82d342e7f967
Martijn Kooijman <martijn.kooijman@hva.nl>
2022-12-04 16:36:10 +0100

    responsive footer en nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,8 @@
 // JavaScript Document
-console.log("hi");
+var menuButton = document.querySelector("header nav button");
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    var deNav = document.querySelector("header nav");
+    deNav.classList.toggle("toonMenu");
+}
